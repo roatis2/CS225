f886f22a1dc2188eff8c7bea7a1ebfa23fb219cc
roatis2 <roatis2@1896fa9e-de69-4edd-9b00-c6635a71d013>
2017-04-15 22:20:20 +0000

    mp6 submission

--- mp6/kdtree.cpp
+++ mp6/kdtree.cpp
@@ -4,9 +4,9 @@
  */
 #include <cmath>
 
-/*Determines if Point a is smaller than Point b in a given dimension d.*/
 template <int Dim>
-bool KDTree<Dim>::smallerDimVal(const Point<Dim>& first, const Point<Dim>& second, int curDim) const
+bool KDTree<Dim>::smallerDimVal(const Point<Dim>& first,
+                                const Point<Dim>& second, int curDim) const
 {
     /**
      * @todo Implement this function!
@@ -20,35 +20,29 @@ bool KDTree<Dim>::smallerDimVal(const Point<Dim>& first, const Point<Dim>& secon
       return false;
 }
 
-
-/*Determines if a Point is closer to the target Point than another reference Point.*/
 template <int Dim>
 bool KDTree<Dim>::shouldReplace(const Point<Dim>& target, const Point<Dim>& currentBest, const Point<Dim>& potential) const
 {
     /**
      * @todo Implement this function!
      */
-     /*if tied, use overloaded < operator*/
-     if(distanceToTarget(target, potential) == distanceToTarget(target, currentBest))
+     if(replaceHelper(target, potential) == replaceHelper(target, currentBest))
       return potential < currentBest;
-     else if(distanceToTarget(target, potential) < distanceToTarget(target, currentBest))
+     else if(replaceHelper(target, potential) < replaceHelper(target, currentBest))
       return true;
     else
       return false;
 }
 
-/*helper function for should replace, finds distance to target for the currentBest and potential above*/
 template <int Dim>
-int KDTree<Dim>::distanceToTarget(const Point<Dim>& target, const Point<Dim>& currentPoint) const
-{
-  /* distance = sqrt((t1 - cp1)^2 + (t2 - cp2)^2 - (t3 - cp3)^2 .... (tk - cpk)^2) */
-  int distanceSquared = 0;
-  for(size_t i = 0; i < Dim; i++){
-    distanceSquared = distanceSquared + pow((target[i] - currentPoint[i]),2);
-  }
-  int distance = sqrt(distanceSquared);
-  return distance;
-
+int KDTree<Dim>::replaceHelper(const Point<Dim>& target, const Point<Dim>& currentPoint)const{ //calculate distance btwn pts
+    /* distance = sqrt((t1 - cp1)^2 + (t2 - cp2)^2 - (t3 - cp3)^2 .... (tk - cpk)^2) */
+    int distanceSquared = 0;
+    for(size_t i = 0; i < Dim; i++){
+      distanceSquared = distanceSquared + pow((target[i] - currentPoint[i]),2);
+    }
+    int distance = sqrt(distanceSquared);
+    return distance;
 }
 
 template <int Dim>
@@ -58,65 +52,59 @@ KDTree<Dim>::KDTree(const vector<Point<Dim>>& newPoints)
      * @todo Implement this function!
      */
      points = newPoints;
-     buildKDTree(0, 0, points.size() - 1);
+     buildHelper(0, points.size() - 1, 0);
 }
 
 template <int Dim>
-void KDTree<Dim>::buildKDTree(int dim, int left, int right)
-{
-  /*set median*/
-  int median = floor((left + right)/2);
-  /*no range*/
-  if(median == 0)
-    return;
-  /*make partitions*/
-  quickSelect(median, left, right, dim);
-  /*build left*/
-    if (left < median)
-        buildKDTree((dim + 1) % Dim, left, median-1);
-  /*build right*/
-    if (right > median)
-        buildKDTree((dim + 1) % Dim, median + 1, right);
+void KDTree<Dim>::buildHelper(int left, int right, int dim){
+  int mid = (right + left)/2;
+  //if(mid == 0){
+  //  return;
+  //}
+  quickSelect(mid, left, right, dim);
+  if(left < mid){
+      buildHelper(left, mid - 1, (dim+1) % Dim);
+  }
+  if(right > mid){
+    buildHelper(mid + 1, right, (dim+1) % Dim);
+  }
 }
 
 template <int Dim>
-void KDTree<Dim>::quickSelect(int median, int left, int right, int dim)
-{
-  /*mispartition, return*/
-  if(left >= right)
+void KDTree<Dim>::quickSelect(int median, int left, int right, int dim){
+  if(left >= right){
     return;
-  /*set pivot*/
-  int pivot = floor((left + right)/2);
-  pivot = partition(left, right, pivot, dim); //change pivot due to partition
-  if(median == pivot) //range is 0
+  }
+  int pivot = (left + right)/2;
+  pivot = partition(left, right, pivot, dim);
+  if(median == pivot){
         return;
-    else if(median < pivot) //quickSelect left
-        quickSelect(left, pivot - 1, dim, median);
-    else  //quickSelect right
-        quickSelect(pivot + 1, right, dim, median);
+  }
+  else if(median < pivot){
+      quickSelect(median, left, pivot - 1, dim);
+  }
+  else{
+      quickSelect(median, pivot + 1, right, dim);
+  }
 }
 
 template <int Dim>
-int KDTree<Dim>::partition(int left, int right, int pivot, int dim)
-{
-    /*swap points[right] and points[pivot]*/
-   Point<Dim> pivotPoint = points[pivot];
+int KDTree<Dim>::partition(int left, int right, int pivot, int dim){
+   Point<Dim> pivots = points[pivot];
    Point<Dim> temp = points[right];
    points[right] = points[pivot];
    points[pivot] = temp;
-   int holdLeft = left; //hold left values
+   int holdLeft = left;
    for (int i = left; i < right; i++)
    {
-       if (smallerDimVal(points[i], pivotPoint, dim) || points[i] == pivotPoint)
+       if (smallerDimVal(points[i], pivots, dim) || points[i] == pivots)
        {
-           /*swap left and current point*/
-           temp = points[holdLeft];
+           Point<Dim> xtemp = points[holdLeft];
            points[holdLeft] = points[i];
-           points[i] = temp;
+           points[i] = xtemp;
            holdLeft++;
        }
    }
-   /*swap right points and left points*/
    temp = points[right];
    points[right] = points[holdLeft];
    points[holdLeft] = temp;
@@ -129,16 +117,17 @@ Point<Dim> KDTree<Dim>::findNearestNeighbor(const Point<Dim>& query) const
     /**
      * @todo Implement this function!
      */
-     /*initialize variables*/
-     int right = points.size()-1;
-     return nnHelper(0, right, 0, query, points[right/2] );
-
+    int right = points.size()-1;
+    Point<Dim> ret;
+    if(points.size() != 0){
+      ret = nnHelper(0, right, 0, query, ret);
+    }
+    return ret;
 }
 
 template <int Dim>
 Point<Dim> KDTree<Dim>::nnHelper(int left, int right, int dim, const Point<Dim>& query, const Point<Dim>& currBest) const{
     Point<Dim> ret = currBest;
-    bool b1 = false;
     if(left == right){
       if(shouldReplace(query, currBest, points[left])){
         ret = points[left];
@@ -149,29 +138,26 @@ Point<Dim> KDTree<Dim>::nnHelper(int left, int right, int dim, const Point<Dim>&
         return ret;
       }
     }
+
     int mid = (left+right)/2;
-    //Point<Dim> pt = points[mid];
+
     if(smallerDimVal(query, points[mid], dim) && left < mid){
       ret = nnHelper(left, mid - 1, (dim+1) % Dim , query, currBest);
-      b1 = false;
     }
     if(!smallerDimVal(query, points[mid], dim) && mid < right){
       ret = nnHelper(mid + 1, right, (dim+1) % Dim , query, currBest);
-      b1 = true;
     }
     if(shouldReplace(query, ret, points[mid])){
         ret = points[mid];
     }
-    int dis = distanceToTarget(query, ret);
+    int dis = replaceHelper(query, ret);
       if((query[dim] - points[mid][dim]) * (query[dim] - points[mid][dim]) <= dis){
-          if(b1 == true && left < mid){
+          if(left < mid){
             ret = nnHelper(left, mid - 1, (dim+1) % Dim , query, ret);
           }
-          if(b1 == false && mid < right){
+          if(mid < right){
             ret = nnHelper(mid + 1, right, (dim+1) % Dim , query, ret);
           }
       }
-
-
     return ret;
 }
--- mp6/maptiles.cpp
+++ mp6/maptiles.cpp
@@ -3,18 +3,41 @@
  * Code for the maptiles function.
  */
 
-#include <iostream>
-#include <map>
-#include "maptiles.h"
+ #include <iostream>
+ #include <map>
+ #include "maptiles.h"
 
-using namespace std;
+ using namespace std;
 
-MosaicCanvas* mapTiles(SourceImage const& theSource,
-                       vector<TileImage> const& theTiles)
-{
-    /**
-     * @todo Implement this function!
-     */
-    return NULL;
-}
+ MosaicCanvas* mapTiles(SourceImage const& theSource, vector<TileImage> const& theTiles)
+ {
+     /**
+      * @todo Implement this function!
+      */
+    vector< Point<3> > colors;
+    int rows = theSource.getRows();
+    int columns = theSource.getColumns();
+ 		MosaicCanvas* canvas = new MosaicCanvas(rows, columns);
+ 	  map<Point<3>, TileImage> tiles;
+    Point<3> point;
+    RGBAPixel pixel;
 
+    for(size_t i = 0; i<theTiles.size(); i++ ){
+ 		   pixel = theTiles[i].getAverageColor();
+ 		   point = Point<3>(pixel.red, pixel.green, pixel.blue);
+ 		   colors.push_back(point);
+ 		   tiles[point] = theTiles[i];
+ 	   }
+
+ 	   KDTree<3> tree(colors);
+
+     for(int i = 0; i<rows; i++){
+ 		     for(int j = 0; j<columns; j++){
+ 			       pixel = theSource.getRegionColor(i,j);
+ 			       point = Point<3>(pixel.red, pixel.green, pixel.blue);
+ 			       point = tree.findNearestNeighbor(point);
+ 			       canvas->setTile(i, j, tiles[point]);
+ 		      }
+ 	     }
+ 	    return canvas;
+ }
