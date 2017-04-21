b3c7f10e5710d7113e05d82c65c28b9b0416ca98
roatis2 <roatis2@1896fa9e-de69-4edd-9b00-c6635a71d013>
2017-04-21 18:58:38 +0000

    mp7.1 submissioni

--- mp7/dsets.h
+++ mp7/dsets.h
@@ -1,1 +1,22 @@
 /* Your code here! */
+#ifndef DSETS_H
+#define DSETS_H
+#include <vector>
+
+using namespace std;
+
+class DisjointSets
+{
+  public:
+    /*Creates n unconnected root nodes at the end of the vector*/
+    void addelements(int num);
+    /*This function should compress paths and works as described in lecture. */
+    int find(int elem);
+    /*makes union*/
+    void setunion(int a, int b);
+
+  private:
+    vector <int> dSets;
+};
+
+#endif
