944f37a5520ae5db97fd4325edaf56f7a6d35daa
roatis2 <roatis2@1896fa9e-de69-4edd-9b00-c6635a71d013>
2017-04-21 18:58:38 +0000

    mp7.1 submissioni

--- roatis2/mp7/dsets.cpp
+++ roatis2/mp7/dsets.cpp
@@ -1,1 +1,45 @@
 /* Your code here! */
+#include <vector>
+#include "dsets.h"
+
+/*Creates n unconnected root nodes at the end of the vector*/
+void DisjointSets::addelements(int num)
+{
+  for(int i = 0; i < num; i++)
+    dSets.push_back(-1);
+  return;
+}
+/*This function should compress paths and works as described in lecture*/
+int DisjointSets::find(int elem)
+{
+  if(dSets[elem] < 0)
+    return elem;
+  else
+  {
+    dSets[elem] = find(dSets[elem]);
+    return dSets[elem];
+  }
+}
+/*makes union*/
+void DisjointSets::setunion(int a, int b)
+{
+  int rootA = find(a);
+  int rootB = find(b);
+  if(rootA == rootB)
+    return;
+
+  int unionSize = dSets[rootA] + dSets[rootB];
+
+  if(dSets[rootA] > dSets[rootB])
+  {
+    dSets[rootA] = rootB;
+    dSets[rootB] = unionSize;
+  }
+
+  else
+  {
+    dSets[rootB] = rootA;
+    dSets[rootA] = unionSize;
+  }
+  return;
+}
