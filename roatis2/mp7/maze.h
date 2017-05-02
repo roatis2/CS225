5b35719eb729a2385d5858d5038aaf6e3690fdc7
roatis2 <roatis2@1896fa9e-de69-4edd-9b00-c6635a71d013>
2017-05-02 04:39:53 +0000

    mp7 submission

--- roatis2/mp7/maze.h
+++ roatis2/mp7/maze.h
@@ -1,1 +1,40 @@
 /* Your code here! */
+#ifndef MAZE_H
+#define MAZE_H
+
+#include "png.h"
+#include "rgbapixel.h"
+#include "dsets.h"
+#include <vector>
+
+using namespace std;
+
+class SquareMaze
+{
+public:
+  SquareMaze();
+  ~SquareMaze();
+  void makeMaze(int width, int height);
+  bool canTravel(int x, int y, int dir) const;
+  void setWall(int x, int y, int dir, bool exists);
+  vector<int> solveMaze();
+  PNG * drawMaze() const;
+  PNG * drawMazeWithSolution();
+
+private:
+  int h;
+  int w;
+  int max;
+  void travel_maze(int &x, int &y, int &length);
+  struct dBool
+  {
+    dBool();
+    bool rightWall;
+    bool bottomWall;
+    bool haveVisited;
+  };
+  dBool ** maze;
+  void clear();
+};
+
+#endif
